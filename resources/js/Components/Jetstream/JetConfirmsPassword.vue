1e6ef77fcd057002c70441f21dcacd5d5c6e5360
secondman <secondmanveran@users.noreply.github.com>
2022-02-15 23:29:49 +0000

    chore(style): apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/js/Components/Jetstream/JetConfirmsPassword.vue
+++ resources/js/Components/Jetstream/JetConfirmsPassword.vue
@@ -81,8 +81,7 @@ defineExpose({ password })
             placeholder="Password"
             ref="password"
             v-model="form.password"
-            @keyup.enter="confirmPassword"
-          />
+            @keyup.enter="confirmPassword" />
 
           <JetInput-error :message="form.error" class="mt-2" />
         </div>
@@ -95,8 +94,7 @@ defineExpose({ password })
           class="ml-2"
           @click="confirmPassword"
           :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
+          :disabled="form.processing">
           {{ button }}
         </JetButton>
       </template>
