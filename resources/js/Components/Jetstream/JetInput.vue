c17f5a41db623e79e31aaf005590f4e0b5e8f2da
secondman <secondmanveran@users.noreply.github.com>
2022-03-31 12:18:07 +0000

    chore(style): apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/js/Components/Jetstream/JetInput.vue
+++ resources/js/Components/Jetstream/JetInput.vue
@@ -16,6 +16,5 @@ defineExpose({ input })
     class="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
     :value="props.modelValue"
     @input="emit('update:modelValue', $event.target.value)"
-    ref="input"
-  />
+    ref="input" />
 </template>
