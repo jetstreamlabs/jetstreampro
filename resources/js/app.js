32984830e5cc638d37b2f99b1f0ded35203e376f
secondmanveran <secondman@jetstreamlabs.com>
2022-11-11 18:06:38 -0700

    fix: properly set app name dependent on whitespace
    Signed-off-by: secondmanveran <secondman@jetstreamlabs.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,14 @@ import '../css/app.css'
 import { createJetstreamApp } from '@/Application'
 import { InertiaProgress } from '@inertiajs/progress'
 
-createJetstreamApp({ appName: import.meta.env.VITE_APP_NAME })
+// Github actions strips quotes from secrets so any app name
+// with whitespace stored in a secret will not parse when copied
+// to a .env file. Thusly we can't use the env to get our app name.
+
+// If your app has no whitespace you can switch these.
+//const appName = import.meta.env.VITE_APP_NAME
+const appName = 'Jetstream Pro'
+
+createJetstreamApp({ appName: appName })
 
 InertiaProgress.init({ color: 'rgb(104, 117, 245)' })
